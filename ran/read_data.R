cee4b9c3e7424fd426891a307eb5683aab236ec2
biran1990 <biran@iastate.edu>
2015-04-07 14:58:08 -0500

    read_data

---
+++ ran/read_data.R
@@ -0,0 +1,3 @@
+setwd("//Users/Ran/Documents/ISU/dmc2015/")
+train <- read.table("data/raw_data/DMC_2015_orders_train.txt", head = T, sep = "|")
+class <- read.table("data/raw_data/DMC_2015_orders_class.txt", head = T, sep = "|")
