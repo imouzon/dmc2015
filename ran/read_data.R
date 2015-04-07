0fbc3015a675e995195f1d9bf4e6d913d99efecd
biran1990 <biran@iastate.edu>
2015-04-07 15:20:49 -0500

    read_data

--- ran/read_data.R
+++ ran/read_data.R
@@ -1,3 +1,11 @@
 setwd("//Users/Ran/Documents/ISU/dmc2015/")
 train <- read.table("data/raw_data/DMC_2015_orders_train.txt", head = T, sep = "|")
 class <- read.table("data/raw_data/DMC_2015_orders_class.txt", head = T, sep = "|")
+
+names(train)
+summary(train)
+attach(train)
+nlevels(orderTime)
+train[orderTime %in% names(table(orderTime)[table(orderTime) == 2]),]
+
+nlevels(train$couponID1)
