438d213ba6fec6dc7029cc57602cd853c345cb5b
Pete <epwalsh10@gmail.com>
2015-04-12 14:51:47 -0500

    Stuff

--- pete/01_exploratory.R
+++ pete/01_exploratory.R
@@ -16,7 +16,8 @@ train$time_diff = train$orderTime - train$couponsReceived
 
 # create variable for difference in times (minutes)
 train$minute_diff = as.numeric(train$time_diff) / 60
-train$day_diff = (ceiling_date(train$orderTime, "day") - ceiling_date(train$couponsReceived, "day")) / (3600*24)
+train$day_diff = (ceiling_date(train$orderTime, "day") - 
+		  ceiling_date(train$couponsReceived, "day")) / (3600*24)
 
 # create variable for day of the week, for both recieved and order time
 train$wday_orderTime <- wday(train$orderTime, label=T)
@@ -24,16 +25,20 @@ train$wday_couponsReceived <- wday(train$couponsReceived, label=T)
 
 qplot(train$minute_diff, geom="histogram", colour=I("white"))
 
-train %>% ggplot(aes(x = wday_orderTime, fill = wday_couponsReceived)) + geom_bar()
-train %>% ggplot(aes(x = wday_couponsReceived, fill = wday_orderTime)) + geom_bar()
+train %>% ggplot(aes(x = wday_orderTime, fill = wday_couponsReceived)) + 
+	geom_bar()
+train %>% ggplot(aes(x = wday_couponsReceived, fill = wday_orderTime)) + 
+	geom_bar()
 
-# every single person who spent their coupon on Tuesday had recieved their coupon on Tuesday, the same day
+# every single person who spent their coupon on Tuesday had recieved their 
+# coupon on Tuesday, the same day
 wday_sum <- train %>% group_by(wday_couponsReceived, wday_orderTime) %>% 
   summarize(count = length(orderID))
 
 # most coupons are sent out on Tuesday
 # all coupons expire before Tuesday
-train %>% ggplot(aes(x = wday_couponsReceived, fill = factor(day_diff))) + geom_bar()
+train %>% ggplot(aes(x = wday_couponsReceived, fill = factor(day_diff))) + 
+	geom_bar()
 
 
 
@@ -47,7 +52,8 @@ sum(train$coupon2Used)
 sum(train$coupon3Used)
 
 train$coupons_used = train$coupon1Used + train$coupon2Used + train$coupon3Used
-train %>% ggplot(aes(x = wday_couponsReceived, fill = factor(coupons_used))) + geom_bar()
+train %>% ggplot(aes(x = wday_couponsReceived, fill = factor(coupons_used))) + 
+	geom_bar()
 
 usage = train %>% group_by(wday_couponsReceived) %>%
   summarize(none = round(sum(coupons_used == 0) / length(coupons_used), 2),
@@ -72,7 +78,6 @@ levels(train$userID) <- as.character(1:length(levels(train$userID)))
 # ==================================================================
 summary(train$basePrice1+train$reward1-train$price1)
 
-# still no idea
 
 
 
