f2519f69225c2f381c64eaf07e5e83a9d7fb66a1
jsaporta <jsaporta@iastate.edu>
2015-04-17 17:04:53 -0500

    found coupons associated with multiple orders with the same basket value

--- jason/dmc_coupon_vals.R
+++ jason/dmc_coupon_vals.R
@@ -9,12 +9,12 @@ library(tidyr)
 ## Reading in the data, trn for "training", tst for "test".
 ## Make the dates nicer with lubridate functions, and
 ## turn the data frames into tbl_dfs to make things sane.
-trn = read.delim("DMC_2015_orders_train.txt",
+trn = read.delim("../data/raw_data/DMC_2015_orders_train.txt",
   stringsAsFactors = FALSE, sep = "|", quote = "") %>%
   mutate(orderTime = ymd_hms(orderTime),
          couponsReceived = ymd_hms(couponsReceived)) %>%
   tbl_df()
-tst = read.delim("DMC_2015_orders_class.txt",
+tst = read.delim("../data/raw_data/DMC_2015_orders_class.txt",
   stringsAsFactors = FALSE, sep = "|", quote = "") %>%
   mutate(orderTime = ymd_hms(orderTime),
          couponsReceived = ymd_hms(couponsReceived)) %>%
@@ -44,8 +44,10 @@ for (i in 1:length(trn2$used)) {
 trn2 <- trn2 %>%
   filter(used == 1) %>%
   group_by(couponID) %>%
-  summarize(upper.bound = min(basketValue)) %>%
-  arrange(upper.bound)
+  summarize(upper.bound = min(basketValue),
+            count = n(),
+            unique.basket.vals = n_distinct(basketValue)) %>%
+  arrange(desc(unique.basket.vals))
 
 ## Make a plot
 plot.me <- trn2 %>%
@@ -55,3 +57,8 @@ jpeg('upper_bounds.jpg')
 ggplot(data = plot.me, aes(x = upper.bound)) +
   geom_histogram(binwidth = 17)
 dev.off()
+
+## Which coupons are associated with multiple orders that have
+## the same basket value?
+same.val <- with(trn2, which(unique.basket.vals != count))
+trn2[same.val, ]
