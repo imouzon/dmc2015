6b3cd59ec8bd7b640ceee26d52f7532f1fa101a3
epwalsh <epwalsh10@gmail.com>
2015-04-25 13:04:36 -0500

    data clean

---
+++ pete/02_coupons.R
@@ -0,0 +1,39 @@
+source("read_data.R")
+
+
+coupon1 = train[,c("coupon1Used", "couponID1")]
+coupon2 = train[,c("coupon2Used", "couponID2")]
+coupon3 = train[,c("coupon3Used", "couponID3")]
+
+names(coupon1) <- c("used", "coupon")
+names(coupon2) <- c("used", "coupon")
+names(coupon3) <- c("used", "coupon")
+
+coupon1$coupon <- as.character(coupon1$coupon)
+coupon1$place <- 1
+coupon2$coupon <- as.character(coupon2$coupon)
+coupon2$place <- 2
+coupon3$coupon <- as.character(coupon3$coupon)
+coupon3$place <- 3
+
+couponData = rbind(coupon1, coupon2, coupon3)
+couponData$coupon = as.factor(couponData$coupon)
+levels(couponData$coupon) = 1:length(levels(couponData$coupon))
+
+couponSummary = couponData %>% group_by(coupon, place) %>%
+	summarize(count = length(used),
+						used = sum(used))
+
+# get all coupons that appeared in every order
+tab = table(couponSummary$coupon)
+ids = as.numeric(which(tab == 3))
+
+index = rep(NA, 3*length(ids))
+j = 1
+for (i in ids) {
+	index[j:(j+2)] = which(couponSummary$coupon == i)
+	j = j + 3
+}
+
+couponSum2 = couponSummary[index,]
+couponSum2$rate = couponSum2$used / couponSum2$count
--- pete/read_data.R
+++ pete/read_data.R
@@ -11,6 +11,7 @@ train$time_diff = train$orderTime - train$couponsReceived
 train$minute_diff = as.numeric(train$time_diff) / 60
 train$day_diff = (ceiling_date(train$orderTime, "day") - 
 		  ceiling_date(train$couponsReceived, "day")) / (3600*24)
+train$day_diff = as.numeric(train$day_diff)
 
 # create variable for day of the week, for both recieved and order time
 train$wday_orderTime <- wday(train$orderTime, label=T)
@@ -18,39 +19,11 @@ train$wday_couponsReceived <- wday(train$couponsReceived, label=T)
 
 train$coupons_used = train$coupon1Used + train$coupon2Used + train$coupon3Used
 
-coupon1 = train[,c("coupon1Used", "couponID1")]
-coupon2 = train[,c("coupon2Used", "couponID2")]
-coupon3 = train[,c("coupon3Used", "couponID3")]
-
-names(coupon1) <- c("used", "coupon")
-names(coupon2) <- c("used", "coupon")
-names(coupon3) <- c("used", "coupon")
-
-coupon1$coupon <- as.character(coupon1$coupon)
-coupon1$place <- 1
-coupon2$coupon <- as.character(coupon2$coupon)
-coupon2$place <- 2
-coupon3$coupon <- as.character(coupon3$coupon)
-coupon3$place <- 3
-
-couponData = rbind(coupon1, coupon2, coupon3)
-couponData$coupon = as.factor(couponData$coupon)
-levels(couponData$coupon) = 1:length(levels(couponData$coupon))
-
-couponSummary = couponData %>% group_by(coupon, place) %>%
-	summarize(count = length(used),
-						used = sum(used))
-
-# get all coupons that appeared in every order
-tab = table(couponSummary$coupon)
-ids = as.numeric(which(tab == 3))
-
-index = rep(NA, 3*length(ids))
-j = 1
-for (i in ids) {
-	index[j:(j+2)] = which(couponSummary$coupon == i)
-	j = j + 3
-}
-
-couponSum2 = couponSummary[index,]
-couponSum2$rate = couponSum2$used / couponSum2$count
+all_coups = c(as.character(train$couponID1), 
+							as.character(train$couponID2),
+							as.character(train$couponID3))
+all_coups = as.factor(all_coups)
+levels(all_coups) = 1:length(levels(all_coups))
+train$couponID1 = all_coups[1:nrow(train)]
+train$couponID2 = all_coups[(nrow(train) + 1):(2*nrow(train))]
+train$couponID3 = all_coups[(2*nrow(train) + 1):length(all_coups)]
