cbb109ec2923c20a6d9caf65521db8dee036c408
Pete <epwalsh10@gmail.com>
2015-05-12 17:16:03 -0500

    condtional RF's

--- pete/08_universal_features.R
+++ pete/08_universal_features.R
@@ -1,12 +1,14 @@
 # ============================================================================
 # Author: Evan P. Walsh
 # Contact: epwalsh@iastate.edu
-# Date: DD-MM-2015
+# Date: May 5, 2015
 #
-# Purpose: Create universal features.
+# Purpose: Create universal features, i.e. features that do not depend on the 
+# response variables.
 #
 # ============================================================================
 
+# Read in my coupon frequency features and write them to an RDS as one dataframe
 train <- read.csv("~/GitHub/dmc2015/features/feature_files/csv/nCoupTrain.csv")
 class <- read.csv("~/GitHub/dmc2015/features/feature_files/csv/nCoupClass.csv")
 d <- rbind(train, class)
---
+++ pete/09_conditionalRF.R
@@ -0,0 +1,12 @@
+# ============================================================================
+# Author: Evan P. Walsh
+# Contact: epwalsh@iastate.edu
+# Date: May DD, 2015
+#
+# Purpose: Create predictions using conditional random forests for 
+# individual coupon predictions, basket value, and basket value using coupon 
+# predictions. Conditional RF's are implemented through the "party" package.
+#
+# ============================================================================
+
+
