2b110150e8e017a3d652906478f3168548124c61
imouzon <imouzon@iastate.edu>
2015-05-14 17:05:12 -0500

    adding set2, set3 feature matrices

--- features/feature_files/R/clean_coupon.r
+++ features/feature_files/R/clean_coupon.r
@@ -3,7 +3,7 @@
 #  Purpose:
 #
 #  Creation Date: 14-05-2015
-#  Last Modified: Thu May 14 15:51:48 2015
+#  Last Modified: Thu May 14 17:04:21 2015
 #  Created By:
 #
 #--------------------------------------**--------------------------------------#
@@ -15,7 +15,8 @@
 
 feature_root = "~/dmc2015/features/feature_files/"
 setwd(feature_root)
-source("./R/combine_set.R")
+source("./universal/combine_universal_features.R")
+uf = combine_universal_features = function(read_dir = "./universal/individual", write_dir = "./universal/combined", universal_clean = "../../data/clean_data/universalCleanData.rds", exclude = c("basePrice_price_ratio.rds"), joinby = "orderID")
 
 ## Making Set 1:
 # combine_set_individual("set1",feature_root,"wide","orderID")
@@ -36,38 +37,13 @@ w3 = readRDS("./set3/set3Combined_wide.rds")
 l3 = readRDS("./set3/set3Combined_long.rds")
 
 
-## Working with wide versions
-dropcols = c("orderTime",
-         "userID",
-         "couponsReceived",
-         "couponID1",
-         "couponID2",
-         "couponID3",
-         "couponsReceivedDate",
-         "orderTimeDate",
-         "batchID",
-         "couponsExpire",
-         "couponsSent")
 
-f1 = uf[,!(names(uf) %in% dropcols)] %>% left_join(w1,by="orderID") %>% arrange(orderID)
-f1 = f1[,!(grepl("ntimes_",names(f1)) | grepl("timesNotUsed_",names(f1)) | grepl("timesUsed_",names(f1)))]
+source("./R/makeWideFeatureMatrix.r")
 
-HTV1 = readRDS("~/dmc2015/data/featureMatrix/HTVset1.rds")
+#####
+makeWideFeatureMatrix(w1,"set1")
+makeWideFeatureMatrix(w2,"set2")
+makeWideFeatureMatrix(w3,"set3")
 
-#train
-Xtrn = HTV1$T %>% select(orderID) %>% left_join(f1,by="orderID") %>% arrange(orderID)
-trn = list("X" = Xtrn %>% select(-orderID,-coupon1Used,-coupon2Used,-coupon3Used,-basketValue),
-           "y" = Xtrn %>% select(orderID,coupon1Used, coupon2Used, coupon3Used, basketValue))
 
-#val
-Xval = HTV1$V %>% select(orderID) %>% left_join(f1,by="orderID") %>% arrange(orderID)
-val = list("X" = Xval %>% select(-orderID,-coupon1Used,-coupon2Used,-coupon3Used,-basketValue),
-           "y" = Xval %>% select(orderID,coupon1Used, coupon2Used, coupon3Used, basketValue))
-
-#class
-Xcls = HTV1$C %>% select(orderID) %>% left_join(f1,by="orderID") %>% arrange(orderID)
-cls = list("X" = Xcls %>% select(-orderID,-coupon1Used,-coupon2Used,-coupon3Used,-basketValue),
-           "y" = Xcls %>% select(orderID,coupon1Used, coupon2Used, coupon3Used, basketValue))
-
-Fmat1 = list("train" = trn, "class" = cls, "validation" = val)
-saveRDS(Fmat1,file="~/dmc2015/data/featureMatrix/featMat_based-on-HTV1_ver0.rds")
+##### make long sets
