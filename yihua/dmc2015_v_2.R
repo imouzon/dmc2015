6b6c550e0c898d0a7213d4d2e6489202d462ffc7
Dickeybird06 <yihuali@iastate.edu>
2015-05-14 14:35:18 -0500

    yihua

--- yihua/dmc2015_v_2.R
+++ yihua/dmc2015_v_2.R
@@ -15,7 +15,7 @@ library(fitdistrplus)
 
 setwd('/Users/yihuali/Documents/dmc2015')
 
-dat <- readRDS('./data/featureMatrix/HTVset1.rds')
+dat <- readRDS('./data/featureMatrix/HTVset3.rds')
 H <- dat$H
 T <- dat$T
 V <- dat$V
@@ -27,11 +27,38 @@ C_melt <- stackCoupons2(C, idcols = c(1:4, 32:49))
 
 Feature <- addFeatures_HTV(H_melt, T_melt, V_melt)
 
-rf1 <- randomForest(x=Feature$T_melt[,c(34,36:411)], 
+any(is.na(Feature$H_melt))
+any(is.na(Feature$T_melt))
+any(is.na(Feature$V_melt))
+names(Feature$H_melt)[80]
+Feature$H_melt <- Feature$H_melt[,-80]
+Feature$T_melt <- Feature$T_melt[,-80]
+Feature$V_melt <- Feature$V_melt[,-80]
+N <- ncol(Feature$H_melt)
+for (i in 1:N) {
+  if (length(unique(Feature$H_melt[,i])) == 1 ||
+        length(unique(Feature$H_melt[,i])) == 1 ||
+        length(unique(Feature$H_melt[,i])) == 1) {
+    Feature$H_melt <- Feature$H_melt[,-i]
+    Feature$T_melt <- Feature$T_melt[,-i]
+    Feature$V_melt <- Feature$V_melt[,-i]
+  }
+}
+
+rf1 <- randomForest(x=Feature$T_melt[,c(34,36:404)], 
                     y=as.factor(Feature$T_melt$couponUsed), 
-                    xtest=Feature$V_melt[,c(34,36:411)], 
+                    xtest=Feature$V_melt[,c(34,36:404)], 
                     ytest=as.factor(Feature$V_melt$couponUsed), 
                     ntree=500, mtry=120)
-table(rf1$test$predicted, as.factor(Feature$V_melt$couponUsed))
-(3169+58)/(3169+58+104+704)
+rf1$confusion
+rf1$test$confusion
+x <- table(as.factor(Feature$V_melt$couponUsed), rf1$test$predicted)
+1 - sum(diag(x))/sum(x)
+
 varImpPlot(rf1)
+y = as.vector(rf1$importance)
+names(y)=rownames(rf1$importance)
+y <- sort(y, decreasing=TRUE)
+head(y,100)
+
+saveRDS(Feature, './yihua/HTVmelt3_Combn_UniqueUser.rds')
