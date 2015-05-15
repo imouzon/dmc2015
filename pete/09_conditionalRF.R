ca18889c96315c99eec5c59a64ce095640d52d9a
epwalsh <epwalsh10@gmail.com>
2015-05-14 22:43:04 -0500

    first try conditional RF

--- pete/09_conditionalRF.R
+++ pete/09_conditionalRF.R
@@ -1,7 +1,11 @@
 # ============================================================================
+# File Name: 09_conditionalRF.R
+#
 # Author: Evan P. Walsh
 # Contact: epwalsh@iastate.edu
-# Date: May DD, 2015
+#
+# Creation Date: 14-05-2015
+# Last Modified: Thu May 14 22:42:43 2015
 #
 # Purpose: Create predictions using conditional random forests for 
 # individual coupon predictions, basket value, and basket value using coupon 
@@ -13,4 +17,24 @@
 #
 # ============================================================================
 
+install.packages("party")
+library(party)
+
+# Historical set 1
+# ----------------------------------------------------------------------------
+h1 <- readRDS("~/GitHub/dmc2015/data/featureMatrix/featMat_based-on-HTVset1_LONG_ver0.rds")
+d <- cbind(couponUsed = h1$train$y$couponUsed, h1$train$X)
+
+h1ct <- ctree(couponUsed~., data = d)
+
+
+
+# Historical set 2
+# ----------------------------------------------------------------------------
+h2 <- readRDS("~/GitHub/dmc2015/data/featureMatrix/featMat_based-on-HTVset2_LONG_ver0.rds")
+
+
+
+# Historical set 3
+# ----------------------------------------------------------------------------
 
