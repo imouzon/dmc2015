8a52206264685769e7d3ad143f2604c7263e2eb3
weicheng <weich@iastate.edu>
2015-05-15 12:43:08 -0500

    bsktLoss function

--- weicheng/loss.R
+++ weicheng/loss.R
@@ -1,49 +1,28 @@
+library(dplyr)
 dat = readRDS("../data/clean_data/clean_data_v1.rds")
 trn0 = dat$train
 trn = trn0[trn0$basketValue<100000,]
+trn = trn[order(trn$userID),]
 
 trn1 = trn[as.numeric(trn$batchID)<9,]
 tst1 = trn[as.numeric(trn$batchID)==9,]
 
-bsv.users = aggregate(basketValue~userID, mean, data=trn1)
-## unique users
-users = unique(trn1$userID)
-userlist = list()
-basketValueUnique = c()
-
-for(i in 1:length(users)){
-    userlist[[i]]=trn1[trn1$userID==users[i],]
-    basketValueUnique = c(basketValueUnique, userlist[[i]]$basketValue[1])
+bsktLoss <- function(trn, tst, FUN=mean){
+    trn = trn[order(trn$userID),]
+    ## basketValues for users who occured in the training set
+    bsv.users = aggregate(basketValue~userID, FUN, data=trn)
+    ## basketValues for users who didn't occure in  the training set
+    uid.rle = rle(trn$userID)
+    idx = c(1, cumsum(uid.rle$lengths[-length(uid.rle$lengths)]) +1)
+    ## basketValues the first time users purchase
+    bsktValueFirstTime = trn$basketValue[idx]
+    tst1 = tst[, c("userID"), drop=FALSE]
+    tst1 = left_join(tst1, bsv.users, by = "userID")
+    bsktValueFirstTime.stat = FUN(bsktValueFirstTime)
+    tst1$basketValue[is.na(tst1$basketValue)] = bsktValueFirstTime.stat
+    ## loss
+    sum((tst1$basketValue - tst$basketValue)^2)/mean(tst$basketValue)^2
 }
 
-
-tst2 = tst1[, c("userID"), drop=FALSE]
-tst2 = left_join(tst2, bsv.users, by = "userID")
-
-basketValueUnique.mean = mean(basketValueUnique)
-tst2$basketValue[is.na(tst2$basketValue)] = basketValueUnique.mean
-
-sum((tst2$basketValue - tst1$basketValue)^2)/mean(tst1$basketValue)^2
-## 503.8027
-
-
-## median
-bsv.users = aggregate(basketValue~userID, median, data=trn1)
-## unique users
-users = unique(trn1$userID)
-userlist = list()
-basketValueUnique = c()
-
-for(i in 1:length(users)){
-    userlist[[i]]=trn1[trn1$userID==users[i],]
-    basketValueUnique = c(basketValueUnique, userlist[[i]]$basketValue[1])
-}
-
-tst2 = tst1[, c("userID"), drop=FALSE]
-tst2 = left_join(tst2, bsv.users, by = "userID")
-
-basketValueUnique.median = median(basketValueUnique)
-tst2$basketValue[is.na(tst2$basketValue)] = basketValueUnique.median
-
-sum((tst2$basketValue - tst1$basketValue)^2)/mean(tst1$basketValue)^2
-## 577.9097
+bsktLoss(trn1, tst1, median)
+bsktLoss(trn1, tst1)
