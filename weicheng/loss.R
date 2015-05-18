2aee478928bf5d05c13dd263bc4ce1b5a9e59b5e
Dickeybird06 <yihuali@iastate.edu>
2015-05-17 20:53:55 -0500

    yihua

--- weicheng/loss.R
+++ weicheng/loss.R
@@ -1,5 +1,11 @@
 library(dplyr)
-dat = readRDS("../data/clean_data/clean_data_v1.rds")
+dat = readRDS("./data/clean_data/clean_data_v1.rds")
+dat <- readRDS("./data/featureMatrix/HTVset1.rds")
+trn <- rbind(dat$H, dat$T)
+trn <- subset(trn, basketValue <= 5000)
+tst <- dat$V
+bsktLoss(trn, tst)
+hist(trn$basketValue, 5000)
 trn0 = dat$train
 trn = trn0[trn0$basketValue<100000,]
 trn = trn[order(trn$userID),]
@@ -7,12 +13,14 @@ trn = trn[order(trn$userID),]
 trn1 = trn[as.numeric(trn$batchID)<9,]
 tst1 = trn[as.numeric(trn$batchID)==9,]
 
+Loss_calculator(0,0,0,0,0,0,272.77,tst$basketValue)
+
 bsktLoss <- function(trn, tst, FUN=mean){
     trn = trn[order(trn$userID),]
     ## basketValues for users who occured in the training set
     bsv.users = aggregate(basketValue~userID, FUN, data=trn)
-    ## basketValues for users who didn't occure in  the training set
-    uid.rle = rle(trn$userID)
+    ## basketValues for users who didn't occur in  the training set
+    uid.rle = rle(as.character(trn$userID))
     idx = c(1, cumsum(uid.rle$lengths[-length(uid.rle$lengths)]) +1)
     ## basketValues the first time users purchase
     bsktValueFirstTime = trn$basketValue[idx]
