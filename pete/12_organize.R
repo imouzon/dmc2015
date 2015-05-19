e3acf9a6cd265cb7d79fc241462bdeb91c653ae1
Pete <epwalsh10@gmail.com>
2015-05-19 02:03:59 -0500

    vars

---
+++ pete/12_organize.R
@@ -0,0 +1,16 @@
+# ============================================================================
+# File Name: 12_organize.R
+#
+# Author: Evan P. Walsh
+# Contact: epwalsh@iastate.edu
+#
+# Creation Date: 19-05-2015
+# Last Modified: Tue May 19 02:03:47 2015
+#
+# Purpose: Clean up predictions and what not.
+#
+# ============================================================================
+
+vars <- readRDS("~/GitHub/dmc2015/pete/predictions/importance_H3_0.8.rds")
+out <- vars[1:150,]
+saveRDS(out, "~/GitHub/dmc2015/penglh/imp_set1/imp_crf_col_name.rds")
