c57ae5834f6c306fdd320b7f00ddc0d295905bd0
ALShum <MKULTRA@ashum-macbook.student.iastate.edu>
2015-05-18 22:58:39 -0500

    adaboost predictions

--- Alex/fitting.R
+++ Alex/fitting.R
@@ -16,7 +16,7 @@ b_wide = readRDS("/Users/MKULTRA/Documents/dmc2015/data/featureMatrix/featMat_ba
 c = readRDS("/Users/MKULTRA/Documents/dmc2015/data/featureMatrix/featMat_based-on-HTVset3_LONG_ver0.3.rds")
 c_wide = readRDS("/Users/MKULTRA/Documents/dmc2015/data/featureMatrix/featMat_based-on-HTVset3_WIDE_ver0.3.rds")
 
-
+#returns the training set from the RDS file
 get_set = function(dat, columns) {
 	trx = dat$train$X
 	try = dat$train$y
@@ -29,6 +29,7 @@ get_set = function(dat, columns) {
 	full
 }
 
+#returns the validation set from the RDS file
 pred_set = function(dat, columns) {
 	vx = dat$validation$X
 	vy = dat$validation$y
@@ -40,10 +41,12 @@ pred_set = function(dat, columns) {
 	full = data.frame(couponUsed = vy$couponUsed, vx)
 }
 
+#simple function wrapper for fitting a model
 fit_data = function(dat, fn, formula, ...) {
 	fn(formula, data = dat, ...)
 }
 
+#ian's function to calculate loss
 lossFunMethod = function(yval,Xval,method,...) {
   hatmat = as.matrix(matrix(predict(method,newdata = Xval,type="response",...),ncol=3,byrow=TRUE))
   ymat = matrix(yval,ncol=3,byrow=TRUE)
@@ -58,17 +61,27 @@ lossFunMethod = function(yval,Xval,method,...) {
 
 
 run_set1_trials = function(method, colset = 1, ...) {
-	bf1 = readRDS("./penglh/imp_rf_col.rds")$col_name
-	bf2 = readRDS("./penglh/imp_lasso_col_name.rds")
-	bf3 = readRDS("./penglh/imp_c50_col_name.rds")
-	bf4 = names(readRDS("./penglh/imp_ada.rds"))
-	master_set = bf1 %>% intersect(bf2) %>% intersect(bf3) %>% intersect(bf4)
+	#bf1 = readRDS("./penglh/imp_rf_col.rds")$col_name
+	#bf2 = readRDS("./penglh/imp_lasso_col_name.rds")
+	#bf3 = readRDS("./penglh/imp_c50_col_name.rds")
+	#bf4 = names(readRDS("./penglh/imp_ada.rds"))
+	bf1 = readRDS("penglh/imp_set1_v4/imp_rf_set1_v4.rds")$col_name
+	bf2 = readRDS("penglh/imp_set1_v4/imp_lasso_set1_v4.rds")
+	bf3 = rownames(readRDS("penglh/imp_set1_v4/imp_c50_set1_v4.rds"))
+	bf4 = readRDS("weicheng/data/imp_gbm_set1.rds")
+	bf5 = readRDS("penglh/imp_set1_v4/imp_corr_v4.rds")
+	master_set = bf1 %>% 
+		intersect(bf2) %>% 
+		intersect(bf3) %>% 
+		intersect(bf4) %>% 
+		intersect(bf4)
 
 	if(colset == 1) set = bf1
 	else if(colset == 2) set = bf2
 	else if(colset == 3) set = bf3
 	else if(colset == 4) set = bf4
-	else if(colset == 5) set = -1
+	else if(colset == 5) set = bf5
+	else if(colset == 6) set = -1
 	else set = master_set
 
 	df = get_set(a, set)
@@ -85,6 +98,10 @@ run_set1_trials(gbm, colset = 2, n.trees = 5000, distribution="adaboost")
 run_set1_trials(gbm, colset = 3, n.trees = 5000, distribution="adaboost")
 run_set1_trials(gbm, colset = 4, n.trees = 5000, distribution="adaboost")
 run_set1_trials(gbm, colset = 5, n.trees = 5000, distribution="adaboost")
+run_set1_trials(gbm, colset = 6, n.trees = 5000, distribution="adaboost")
+run_set1_trials(gbm, colset = 7, n.trees = 5000, distribution="adaboost")
+
+
 
 run_set1_trials(gbm, colset = 1, n.trees = 5000)
 run_set1_trials(gbm, colset = 2, n.trees = 5000)
@@ -173,6 +190,13 @@ cforest_imp_c = varimp(cc)
 #saveRDS(cforest_imp, "important_features_set1_long_cforest.rds")
 #saveRDS(cforest_imp_b, "important_features_set2_long_cforest.rds")
 #saveRDS(cforest_imp_c, "important_features_set3_long_cforest.rds")
+
+
+bf = names(tail(sort(cforest_imp_c), 30))
+df = get_set(c, bf)
+df2 = pred_set(c, bf)
+m = cforest(couponUsed~., data = df, control = cforest_unbiased(mtry = 10, ntree = 200))
+lossFunMethod(df2$couponUsed, df2[,-1], m)
 
 
 
