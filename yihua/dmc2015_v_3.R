ef1231ebaaa30f1caab3ed3dc75667471a71fff0
Dickeybird06 <yihuali@iastate.edu>
2015-05-19 03:06:01 -0500

    yihua

--- yihua/dmc2015_v_3.R
+++ yihua/dmc2015_v_3.R
@@ -36,6 +36,9 @@ feature.386 <- readRDS('./penglh/imp_set1/imp_corr_col_name.rds')
 feature.rf150 <- readRDS('./penglh/imp_set1/imp_rf150_col_name.rds')
 feature.lasso <- readRDS('./penglh/imp_set1/imp_lasso_col_name.rds')
 feature.c50 <- readRDS('./penglh/imp_set1/imp_c50_col_name.rds')
+feature.gbm <- readRDS('./penglh/imp_set1/imp_gbm_col_name.rds')
+feature.crf <- readRDS('./penglh/imp_set1/imp_crf_col_name.rds')
+feature.crf <- as.character(feature.crf$var)
 # feature.318 <- as.character(feature.318$col_name)
 # feature.c50 <- readRDS('./penglh/imp_c50_col_name.rds')
 # feature.c50 <- as.character(feature.c50)
@@ -55,7 +58,7 @@ feature.c50 <- readRDS('./penglh/imp_set1/imp_c50_col_name.rds')
 # feature.lasso.v4.set1 <- readRDS('./penglh/imp_set1_v4/imp_lasso_set1_v4.rds')
 # feature.c50.v4.set1 <- readRDS('./penglh/imp_set1_v4/imp_c50_set1_v4.rds')
 
-feature <- intersect(feature.c50, names(train.x))
+feature <- intersect(feature.gbm, names(train.x))
 
 length(feature)
 
@@ -64,7 +67,7 @@ rf <- randomForest(x=train.x[,feature],
                    y=as.factor(train.y$couponUsed),
                    xtest=valid.x[,feature],
                    ytest=as.factor(valid.y$couponUsed),
-                   keep.forest=TRUE, ntree=2000)
+                   keep.forest=TRUE, ntree=500)
 proc.time()-a
 rf.pred <- predict(rf, valid.x[,feature], type="prob")
 rf.pred.T <- predict(rf, class.x[,feature], type="prob")
@@ -95,7 +98,7 @@ sum(loss)
 a <- proc.time()
 rf1 <- randomForest(x=rbind(train.x[,feature], valid.x[,feature]),
                    y=as.factor(c(train.y$couponUsed, valid.y$couponUsed)),
-                   keep.forest=TRUE, ntree=2000)
+                   keep.forest=TRUE, ntree=500)
 proc.time() - a
 rf.pred.TV <- predict(rf1, class.x[,feature], type="prob")
 
@@ -106,7 +109,7 @@ validation <- rf.pred[,2]
 class.T <- rf.pred.T[,2]
 class.TV <- rf.pred.TV[,2]
 err <- sum(loss)
-method <- 'rf_c50'
-file <- './predictions/set1/rf_c50_set1.rds'
+method <- 'rf_gbm'
+file <- './predictions/set1/rf_gbm_set1.rds'
 savePred(d, validation, class.T, class.TV, err, method, file)
   
